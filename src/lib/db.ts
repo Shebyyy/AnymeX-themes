35e976705db5ee5d28f87ab5d9e454759f490c1e
neoniq <demon9836@gmail.com>
2026-04-05 17:53:09 +0300

    fix: lint error

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -219,8 +219,11 @@ class QueryBuilder {
     }
   }
 
-  then(resolve: any, reject: any) {
-    return this.execute().then(resolve, reject);
+  then<TResult1 = any, TResult2 = never>(
+    onfulfilled?: ((value: any) => TResult1 | PromiseLike<TResult1>) | undefined | null,
+    onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | undefined | null
+  ): Promise<TResult1 | TResult2> {
+    return this.execute().then(onfulfilled, onrejected);
   }
 }
 
